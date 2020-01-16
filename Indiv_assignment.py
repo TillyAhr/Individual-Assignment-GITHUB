7da6067d0833ac9438006b152bc6fd7015fc7b7d
Mathilda <mathildaahr1@gmail.com>
2020-01-16 14:20:11 +0100

    Python code that reads json file and creates csv tidy data

--- Indiv_assignment.py
+++ Indiv_assignment.py
@@ -2,3 +2,9 @@ import json
 
 with open('Individual_Assignment_GIT/conflict_indo_2004.json', encoding='utf8') as file:
     conflict_data_indo = json.load(file)
+
+
+with open('Individual_Assignment_GIT/conflict_data_indo.csv', 'w', encoding='utf8') as file:
+    file.write('id, year, conflict_name, country, date_start, best \n')
+    for violence in conflict_data_indo:
+        file.write(f'{violence["id"]}, {violence["year"]}, {violence["conflict_name"]}, {violence["country"]}, {violence["date_start"]}, {violence["best"]} \n')
